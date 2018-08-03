9c00044ac5a89680663b4a1638ac90f510f8c2ea
jakebeal <jakebeal@alum.mit.edu>
2018-08-03 11:13:54 -0700

    making gradle testing work

--- SD2MaintainDictionary/src/test/java/com/bbn/sd2/DictionaryTestShared.java
+++ SD2MaintainDictionary/src/test/java/com/bbn/sd2/DictionaryTestShared.java
@@ -2,13 +2,24 @@ package com.bbn.sd2;
 
 import static org.junit.Assert.*;
 
+import java.io.IOException;
+import java.io.InputStream;
+
 import org.apache.commons.cli.CommandLine;
+import org.apache.commons.io.IOUtils;
 import org.junit.Test;
 
 public class DictionaryTestShared {
 
     public static void initializeTestEnvironment() {
-        String password = System.getenv().get("test_password");
+        String password = null;
+        try {
+            InputStream stream = DictionaryTestShared.class.getResourceAsStream("/password.txt");
+            password = IOUtils.toString(stream);
+        } catch(IOException e) {
+            fail("Can't get password for logging into SynBioHub");
+        }
+
         CommandLine cmd = DictionaryMaintainerApp.parseArguments("-s","15","-p",password,"-l","jakebeal@ieee.org","-c","https://synbiohub.utah.edu/user/jakebeal/scratch_test_collection/","-S","https://synbiohub.utah.edu/");
         DictionaryAccessor.configure(cmd);
         SynBioHubAccessor.configure(cmd);
