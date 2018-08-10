5e55f62efae99d6c9543d28b14867cf246402933
jakebeal <jakebeal@alum.mit.edu>
2018-08-10 10:37:39 -0500

    1.0.0 release

--- SD2MaintainDictionary/src/main/java/com/bbn/sd2/DictionaryMaintainerApp.java
+++ SD2MaintainDictionary/src/main/java/com/bbn/sd2/DictionaryMaintainerApp.java
@@ -5,7 +5,7 @@ import java.util.logging.Logger;
 import org.apache.commons.cli.*;
 
 public class DictionaryMaintainerApp {
-    public static final String VERSION = "1.0.0-alpha";
+    public static final String VERSION = "1.0.0";
     
     private static Logger log = Logger.getGlobal();
     private static int sleepMillis;
