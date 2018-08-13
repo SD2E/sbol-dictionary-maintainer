92241fc1928f6c9140eef4e21ae5da1398b58aee
jakebeal <jakebeal@alum.mit.edu>
2018-08-13 15:01:35 -0500

    one-shot updates

--- SD2MaintainDictionary/src/main/java/com/bbn/sd2/DictionaryMaintainerApp.java
+++ SD2MaintainDictionary/src/main/java/com/bbn/sd2/DictionaryMaintainerApp.java
@@ -9,20 +9,24 @@ public class DictionaryMaintainerApp {
     
     private static Logger log = Logger.getGlobal();
     private static int sleepMillis;
+    private static boolean oneShot = false;
     
     public static void main(String... args) {
         // Parse arguments and configure
         CommandLine cmd = parseArguments(args);
         sleepMillis = 1000*Integer.valueOf(cmd.getOptionValue("sleep","60"));
+        oneShot = cmd.hasOption("one-shot");
+        log.info("Dictionary Maintainer initializing "+(oneShot?"in single update mode":"for continuous operation"));
         DictionaryAccessor.configure(cmd);
         SynBioHubAccessor.configure(cmd);
 
+
         // Run as an eternal loop, reporting errors but not crashing out
-        while(true) {
+        do {
             DictionaryAccessor.restart();
             SynBioHubAccessor.restart();
             
-            while(true) {
+            do {
                 try {
                     MaintainDictionary.maintain_dictionary();
                 } catch(Exception e) {
@@ -30,12 +34,13 @@ public class DictionaryMaintainerApp {
                     e.printStackTrace();
                 }
                 try {
-                    Thread.sleep(sleepMillis);
+                    if(!oneShot) Thread.sleep(sleepMillis);
                 } catch(InterruptedException e) {
                     // ignore sleep interruptions
                 }
-            }
-        }
+            } while(!oneShot);
+        } while(!oneShot);
+        log.info("Dictionary Maintainer run complete, shutting down.");
     }
     
     /**
@@ -52,6 +57,7 @@ public class DictionaryMaintainerApp {
         options.addOption("g", "gsheet_id", true, "Google Sheets ID of spreadsheet");
         options.addOption("S", "server", true, "URL for SynBioHub server");
         options.addOption("f", "spoofing", true, "URL prefix for a test SynBioHub server spoofing as another");
+        options.addOption("o", "one-shot", false, "run only once and then quit");
         
         // Parse arguments
         CommandLine cmd = null;
