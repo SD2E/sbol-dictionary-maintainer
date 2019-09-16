3e07050d518bf066584e37d51bd4a91b25887ad2
Daniel Sumorok <dsumorok@bbn.com>
2019-09-16 16:25:45 -0400

    Bumped version to 1.4.5

--- src/main/java/com/bbn/sd2/DictionaryMaintainerApp.java
+++ src/main/java/com/bbn/sd2/DictionaryMaintainerApp.java
@@ -21,7 +21,7 @@ import org.json.*;
 import org.synbiohub.frontend.SynBioHubException;
 
 public class DictionaryMaintainerApp {
-    public static final String VERSION = "1.4.4";
+    public static final String VERSION = "1.4.5";
 
     private static Logger log = Logger.getGlobal();
     private static int sleepMillis;
